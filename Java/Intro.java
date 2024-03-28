6f494a929aeafae71d0731b6e90393a7c8d438e9
Quinten Krauth <quintenkrauth19@gmail.com>
2024-03-27 20:16:52 -0400

    first commit

---
+++ Java/Intro.java
@@ -0,0 +1,7 @@
+public class Intro {
+    public static void main(String[] args) {
+
+        System.out.println("We are printing this line"); // println makes new line or do "\n"
+        System.out.println("This line we also print");
+    }
+}
---
+++ Java/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int x; // declaration
+        x = 123; // assignment
+
+        int y = 456; // initialization
+
+        System.out.println(x + y);
+        System.out.println("x + y");
+        System.out.println(x + "" + y);
+
+        String name = "Quinten";
+
+        System.out.println(name);
+    }
+}
